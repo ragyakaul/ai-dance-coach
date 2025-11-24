0457adaa4e45d35719dc65933d2dc524c589b613
Ragya Kaul <ragyakaul@gmail.com>
2025-11-24 20:59:13 +1030

    stick figure on dancer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import mediapipe as mp
 import cv2
-
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Input and output paths
 danceVideoPath = "/Users/ragyakaul/Desktop/Code/pythonprojects/aidancecoach"
@@ -17,26 +17,30 @@ pose = mp_pose.Pose()
 mp_drawing_utils = mp.solutions.drawing_utils
 
 
-
 # Open the source video file
 cap = cv2.VideoCapture("dance_video.MOV")
 
 
 while cap.isOpened():
-    ret, frame = cap.read()
+    ret, picture_bgr = cap.read()
     if not ret:
         break
 
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    picture_rgb = cv2.cvtColor(picture_bgr, cv2.COLOR_BGR2RGB)
     
     # Process each frame with mediapipe pose
-    result = pose.process(frame_rgb)
+    result = pose.process(picture_rgb)
 
     if result.pose_landmarks:
         # Media pipe draws a connected skeleton between th  on each frame
-        mp_drawing_utils.draw_landmarks(frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS) 
+        mp_drawing_utils.draw_landmarks(picture_bgr, result.pose_landmarks, mp_pose.POSE_CONNECTIONS) 
     
-    cv2.imshow('MediaPipe Pose', frame)   
+    cv2.imshow('MediaPipe Pose', picture_bgr)   
     if cv2.waitKey(1) & 0xFF == 27:
         break
     
+cap.release()
+cv2.destroyAllWindows()
+
+ 
+         
